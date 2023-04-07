e597ee22cfd0ba429f4af2621d6ecbae774e6b5a
servo <servomekanism@gmail.com>
2023-04-07 16:03:21 +0300

    added noob arg control

--- src/main.rs
+++ src/main.rs
@@ -29,45 +29,72 @@ fn main() {
     // args
     // TODO: check arg number and act accordingly
     let args: Vec<String> = env::args().collect();
-    let names_file_name = &args[1];
-    let surnames_file_name = &args[2];
 
-    // open file to write
-    let newfile = Path::new("wordlist.txt");
-    let display = newfile.display();
+    match args.len() {
+        1 => {
+            println!("No arguments.");
+            help();
+        }
 
-    let mut outfile = match File::create(&newfile) {
-        Err(why) => panic!("couldn't create {}: {}", display, why),
-        Ok(outfile) => outfile,
-    };
+        2 => {
+            println!("Not enough arguments.");
+            help();
+        }
 
-    // read surnames file
-    if let Ok(surnames) = read_lines(surnames_file_name) {
-        for line in surnames {
-            if let Ok(surname) = line {
-                if let Ok(names) = read_lines(names_file_name) {
-                    for nameline in names {
-                        if let Ok(name) = nameline {
-                            match outfile.write_all(surname.as_bytes()) {
-                                Err(why) => panic!("couldn't write to {}: {}", display, why),
-                                Ok(_) => (),
-                            }
-                            match outfile.write_all(b" ") {
-                                Err(why) => panic!("couldn't write to {}: {}", display, why),
-                                Ok(_) => (),
-                            }
-                            match outfile.write_all(name.as_bytes()) {
-                                Err(why) => panic!("couldn't write to {}: {}", display, why),
-                                Ok(_) => (),
-                            }
-                            match outfile.write_all(b"\n") {
-                                Err(why) => panic!("couldn't write to {}: {}", display, why),
-                                Ok(_) => (),
+        3 => {
+            let names_file_name = &args[1];
+            let surnames_file_name = &args[2];
+
+            // open file to write
+            let newfile = Path::new("wordlist.txt");
+            let display = newfile.display();
+
+            let mut outfile = match File::create(&newfile) {
+                Err(why) => panic!("couldn't create {}: {}", display, why),
+                Ok(outfile) => outfile,
+            };
+
+            // read surnames file
+            if let Ok(surnames) = read_lines(surnames_file_name) {
+                for line in surnames {
+                    if let Ok(surname) = line {
+                        if let Ok(names) = read_lines(names_file_name) {
+                            for nameline in names {
+                                if let Ok(name) = nameline {
+                                    match outfile.write_all(surname.as_bytes()) {
+                                        Err(why) => {
+                                            panic!("couldn't write to {}: {}", display, why)
+                                        }
+                                        Ok(_) => (),
+                                    }
+                                    match outfile.write_all(b" ") {
+                                        Err(why) => {
+                                            panic!("couldn't write to {}: {}", display, why)
+                                        }
+                                        Ok(_) => (),
+                                    }
+                                    match outfile.write_all(name.as_bytes()) {
+                                        Err(why) => {
+                                            panic!("couldn't write to {}: {}", display, why)
+                                        }
+                                        Ok(_) => (),
+                                    }
+                                    match outfile.write_all(b"\n") {
+                                        Err(why) => {
+                                            panic!("couldn't write to {}: {}", display, why)
+                                        }
+                                        Ok(_) => (),
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
+        }
+
+        _ => {
+            help();
         }
     }
 }
@@ -78,4 +105,8 @@ where
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn help() {
+    println!("usage: wordlister <names_file> <surnames_file>");
 }
