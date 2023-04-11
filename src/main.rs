674836fc3fb8b17e7a6cad4cd5eb9b0caf62be64
user <user@computer>
2023-04-11 11:56:17 +0300

    better approach

--- src/main.rs
+++ src/main.rs
@@ -21,91 +21,34 @@
 
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::io::Read;
+use std::io::Seek;
+use std::io::{BufRead, BufReader, BufWriter, Write};
 
-fn main() {
-    // todo reimplement arg stuff
+fn main() -> std::io::Result<()> {
+    // todo: add arg checks information
     let args: Vec<String> = env::args().collect();
 
-    match args.len() {
-        1 => {
-            println!("No arguments.");
-            help();
-        }
-
-        2 => {
-            println!("Not enough arguments.");
-            help();
-        }
-
-        3 => {
-            let names_file_name = &args[1];
-            let surnames_file_name = &args[2];
-
-            // open file to write
-            let newfile = Path::new("wordlist.txt");
-            let display = newfile.display();
-
-            let mut outfile = match File::create(&newfile) {
-                Err(why) => panic!("couldn't create {}: {}", display, why),
-                Ok(outfile) => outfile,
-            };
-
-            // read surnames file
-            if let Ok(surnames) = read_lines(surnames_file_name) {
-                for line in surnames {
-                    if let Ok(surname) = line {
-                        if let Ok(names) = read_lines(names_file_name) {
-                            for nameline in names {
-                                if let Ok(name) = nameline {
-                                    match outfile.write_all(surname.as_bytes()) {
-                                        Err(why) => {
-                                            panic!("couldn't write to {}: {}", display, why)
-                                        }
-                                        Ok(_) => (),
-                                    }
-                                    match outfile.write_all(b" ") {
-                                        Err(why) => {
-                                            panic!("couldn't write to {}: {}", display, why)
-                                        }
-                                        Ok(_) => (),
-                                    }
-                                    match outfile.write_all(name.as_bytes()) {
-                                        Err(why) => {
-                                            panic!("couldn't write to {}: {}", display, why)
-                                        }
-                                        Ok(_) => (),
-                                    }
-                                    match outfile.write_all(b"\n") {
-                                        Err(why) => {
-                                            panic!("couldn't write to {}: {}", display, why)
-                                        }
-                                        Ok(_) => (),
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        _ => {
-            help();
-        }
+    if args.len() != 4 {
+        println!("Usage: {} <names.txt> <surnames.txt> <output.txt>", args[0]);
+        return Ok(());
     }
-}
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+    let names = File::open(&args[1])?;
+    let surnames = File::open(&args[2])?;
+    let output_file = File::create(&args[3])?;
 
-fn help() {
-    println!("usage: wordlister <names_file> <surnames_file>");
+    let names_reader = BufReader::new(names);
+    let mut surnames_reader = BufReader::new(surnames);
+    let mut writer = BufWriter::new(output_file);
+
+    for line1 in names_reader.lines() {
+        let line1 = line1?;
+        for line2 in surnames_reader.by_ref().lines() {
+            let line2 = line2?;
+            writer.write_all(format!("{} {}\n", line1, line2).as_bytes())?;
+        }
+        surnames_reader.seek(std::io::SeekFrom::Start(0))?;
+    }
+    Ok(())
 }
